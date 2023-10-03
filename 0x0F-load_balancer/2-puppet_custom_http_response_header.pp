2572dab73fcec22c3feeb88ffcabe07e79df45b0
Youssef Nasrallah <67008509+Ussf-nassrallah@users.noreply.github.com>
2023-10-03 16:20:38 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,24 @@
+# creating a custom HTTP header response, but with Puppet.
 # step 1
 exec { 'update':
   command  => 'sudo apt-get update',
   provider => shell,
 }
-
+->
 # step 2
 package {'nginx':
   ensure => present,
 }
-
+->
 # step 3
 file_line { 'header line':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "    location / {
-    add_header X-Served-By ${hostname};",
+    add_header X-Served-By ${hostname};",  # Add a custom header to the Nginx configuration
   match  => '^\tlocation / {',
 }
-
+->
 # step 4
 exec { 'restart the service':
   command  => 'sudo service nginx restart',
