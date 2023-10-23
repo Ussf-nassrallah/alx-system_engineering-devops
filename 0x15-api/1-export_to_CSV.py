fc516e57e9b8154029fb3d770f45df5b21452ed8
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-10-23 05:21:28 -0700

    export data to csv file

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,8 +18,8 @@ def export_user_data(user_id):
     username = user.get('username')
     filename = user_id + '.csv'
 
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [
                 user_id,
