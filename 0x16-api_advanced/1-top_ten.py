e88a937f931e6107c7f35946e48c502a76b22cc7
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-11-07 13:00:16 +0100

    returns the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" task-1 document """
+import requests
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API
+      and returns the first 10 hot posts listed
+      for a given subreddit
+    """
+    apiUrl = f"https://www.reddit.com/r/{subreddit}/hot/.json"
+
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {"limit": 10}
+
+    res = requests.get(
+        apiUrl,
+        headers=headers,
+        params=params,
+        allow_redirects=False
+    )
+    if res.status_code == 404:
+        print('None')
+        return
+    output = res.json().get("data")
+    for obj in output.get("children"):
+        print(print(obj.get("data").get("title")))
